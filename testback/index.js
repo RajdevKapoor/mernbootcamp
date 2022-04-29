639b8eaebac71e0f30e402a1e626874eea6a0fab
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-28 22:58:31 -0400

    implemented middleware

--- testback/index.js
+++ testback/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+const admin = (req, res) => {
+	res.send('this is admin');
+};
+
+const isAdmin = (req, res, next) => {
+	console.log('is admin is running');
+	next();
+};
+
+app.get('/admin', isAdmin, admin);
+
 app.listen(port, () => {
 	console.log(`Server is up and running on port ${port}`);
 });
