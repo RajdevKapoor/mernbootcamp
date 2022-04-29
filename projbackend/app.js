420cfd912756a4b3dfb8d2eb90dfa1ff29a9ee86
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-28 23:10:41 -0400

    added middleware to app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const port = process.env.PORT || 8000;
 
 mongoose
 	.connect(process.env.DATABASE, {
@@ -14,7 +19,9 @@ mongoose
 		console.log('DB CONNECTED');
 	});
 
-const port = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(port, () => {
 	console.log(`app is running at ${port}`);
