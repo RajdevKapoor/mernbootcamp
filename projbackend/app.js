ced5772075141c736ca157ab0edcf3094fc15478
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-29 23:35:16 -0400

    added /signout

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,8 +7,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const port = process.env.PORT || 8000;
+const authRoutes = require('./routes/auth');
 
+//DB Connection
 mongoose
 	.connect(process.env.DATABASE, {
 		useNewUrlParser: true,
@@ -19,10 +20,18 @@ mongoose
 		console.log('DB CONNECTED');
 	});
 
+//Middlewares
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+//My Routes
+app.use('/api', authRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
 app.listen(port, () => {
 	console.log(`app is running at ${port}`);
 });
