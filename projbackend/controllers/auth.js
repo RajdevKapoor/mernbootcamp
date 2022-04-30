b4fa9523976c08473013d9be4aef11b27c9fbf30
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-30 00:42:57 -0400

    added signup route

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,10 @@
+exports.signup = (req, res) => {
+	console.log('REQ BODY', req.body);
+	res.json({
+		message: 'user signup!'
+	});
+};
+
 exports.signout = (req, res) => {
 	res.json({
 		message: 'user signout'
